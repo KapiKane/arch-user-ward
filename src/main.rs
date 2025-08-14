10fef7322f2e48876cda9734aae96506d7278c66
KapiKane <Lailo_Sux.Still@proton.me>
2025-08-14 11:39:54 -0600

    broken if statements or smtn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let app_query: Vec<String> = std::env::args().collect();
+
+    match args.get(1) {
+        Some(arg) => println!("Got argument: {arg}"),
+        None => println!("No argument provided!"),
+    }
+
+    if app_query.clone().unwrapped() == "-R" {
+        println!("u put in r for review")
+    }
+    if app_query.clone().unwrapped() == "-R" {
+        println!("u put in r for review")
+    }
 }
+
+/* 
+-R = review
+-S = sync/download
+-s = search 
+*/
