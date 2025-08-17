3cf77e6ae882a963ebfa365366adc1cc19a6e423
KapiKane <Lailo_Sux.Still@proton.me>
2025-08-16 20:46:07 -0600

    finished a basic form of the way this'll be structured, gl to the person checking the source control :sob:

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,34 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     match args.get(1) { // index 1 = first argument after program name
-        Some(arg) => println!("Got argument: {}", arg),
+        Some(_arg) => println!(""),
         None => println!("No argument provided!"),
+    }
 
-    if args == "-R" {
-        println!("u put in r for review")
-    }
-    if args == "-S" {
-        println!("u put in r for review")
-    }
+if args.contains(&"-r".to_string()) {
+    review();
+}
+
+if args.contains(&"-i".to_string()) {
+    install();
+}
+
+if args.contains(&"-s".to_string()) {
+    search();
+}
+
+}
+
+fn review() {
+    println!("u put in r for review");
+}
+
+fn install() {
+    println!("u put in i for install");
+}
+
+fn search() {
+    println!("u put in s for search");
 }
 
 // R = Review
